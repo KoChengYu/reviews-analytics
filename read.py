ab90b43ccf1aa72adf7813ae80ad2b585acc3cf0
KoChengYu <bennycyko@gmail.com>
2018-09-17 11:46:11 +0800

    add filter

--- read.py
+++ read.py
@@ -14,3 +14,13 @@ for d in data: #每一個d是一個字串
 	sum_len = sum_len + len(d)
 
 print('平均為', sum_len/len(data), '個字')
+
+
+new = []
+
+for d in data: #for loop = 把data中的東西一個一個拿出來，d是字串，data是清單
+	if len(d) < 100:
+		new.append(d)
+
+print('共有', len(new), '筆留言長度小於100')
+print(new[0])
